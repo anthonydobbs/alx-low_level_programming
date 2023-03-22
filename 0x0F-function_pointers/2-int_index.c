1b7e6d82ae87b425870187be910af7430f5670da
anthonydobbs <anthonydobbiins@gmail.com>
2023-03-22 08:50:42 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_operators.h"
 
 /**
@@ -19,5 +20,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[i]))
 			return (i);
 	}
-		return (-1);
+	return (-1);
 }
