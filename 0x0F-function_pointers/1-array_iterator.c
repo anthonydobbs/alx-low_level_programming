b3b307c5cc1adce89fca2395cac7e626e0470e05
anthonydobbs <anthonydobbiins@gmail.com>
2023-03-22 08:11:50 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,18 +5,18 @@
  * array_iteration - an array that allows the pointer to iterate over functions
  * @array: array
  *@size: size of the array
- *@action: pointer to print 
+ *@action: pointer to print
  *Return: void
  */
-void array_iterator(int *array, size_t size, void (*action)(int)) 
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
 	if (array == NULL || action == NULL)
 		return;
 
-	for (size_t i = 0; i < size; i++) 
+	for (i = 0; i < size; i++)
 	{
-		(*action)(array[i]);
+		(action)(array[i]);
 	}
 }
