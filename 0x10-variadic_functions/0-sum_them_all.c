879412a9286759c4e4dd90a1eaecc055b8d768b6
anthonydobbs <anthonydobbiins@gmail.com>
2023-03-24 08:13:01 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum up all the arguments passed
+ * @n: number of integers
+ * Return: 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arg;
+
+	if (n == 0)
+
+		return (0);
+}
